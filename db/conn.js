b18b7dc908f1dea4f5378d97c7eb40797f6cd5eb
feliipecardosoo <fglibino@gmail.com>
2023-11-13 21:32:27 -0300

    Estrutura de pastas

---
+++ db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('dbIbt', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+try {
+    sequelize.authenticate()
+    console.log('Conectamos no bdIbt')
+} catch(err) {
+    console.log('Nao foi possivel conectar ao banco de dados!')
+    console.log(err)
+}
+
+module.exports = sequelize
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+
+const app = express()
+
+const port = 3000
+const conn = require('./db/conn')
+
+conn
+    .sync()
+    .then(() => {
+        app.listen(port)
+    })
+    .catch((err) => console.log(`Erro ao acessar a Porta!, ${err}`))
